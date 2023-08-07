1b171bda5fe18d4b8e99c1b664f594620bf932ef
hybf1212 <hybf1212@naver.com>
2023-08-07 22:52:02 +0900

    배너 fade 효과 적용

--- js/jq.js
+++ js/jq.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    let count = 0;
+
+    setInterval(function(){
+        count++;
+        count %= 3;
+        $(".slide div").eq(count).addClass("on").siblings().removeClass("on")
+    },3000)
 })
